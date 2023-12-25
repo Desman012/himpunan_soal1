a06c01d543c6bb3512bc7af1e142f921a3d3c31e
Desman Dwi Saputra <desmandwisaputra@gmail.com>
2023-12-25 22:32:20 +0700

    Add files via upload

---
+++ soal1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    string name; 
+    string placeofBirth;
+    cout << "Your name  \t  : ";
+    cin >> name;
+    cout << "Your Place of Birth : ";
+    cin >> placeofBirth;
+
+    cout << "Hello, my name is " << name << " and I born at " << placeofBirth;
+}
